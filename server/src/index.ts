57b47f299127119caff4b675cfda72f07929729c
Govind Thakur <govind8thakur@gmail.com>
2026-04-10 17:23:48 +0530

    feat: add profile and plan routers with basic error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import { profileRouter } from './routes/profile';
+import { planRouter } from './routes/plan';
 
 dotenv.config();
 
@@ -10,9 +12,14 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json);
-app.param;
+app.use(express.json());
+
+//api routes
+app.use("/api/profileRouter", profileRouter);
+app.use("/api/planRouter", planRouter);
 
 app.listen(PORT, () => {
     console.log(`server runing on portt : ${PORT}`);
 });
+
+
