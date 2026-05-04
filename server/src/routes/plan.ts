be720016888cca464ad221f08fdc07aea32b4420
Govind Thakur <govind8thakur@gmail.com>
2026-05-04 17:52:57 +0530

    feat: add user profile validation in plan route

--- server/src/routes/plan.ts
+++ server/src/routes/plan.ts
@@ -1,3 +1,26 @@
 import { Router, type Request, type Response } from "express";
+import { PresentationIcon } from "lucide-react";
+import { error, log } from "node:console";
+import { Prisma } from "../../generated/prisma/browser";
+import { prisma } from "../lib/prisma";
 
 export const planRouter = Router();
+
+planRouter.post("/",async(req:Request,res:Response)=>{
+try {
+    const {userId} = req.body;
+    if(!userId){
+        return res.status(400).json({error: "User id is required"});
+    }
+    const profile = await prisma.user_profiles.findUnique({
+        where:{user_id:userId},
+    });
+    if(!profile){
+        return res.status(400).json({error: "User profile   not found. Complete onBorading first"});
+    }
+    
+} catch (error) {
+    console.log("Error generating plan ", error);
+    res.status(500).json({error:"Failed to generate plan"});
+}
+})
