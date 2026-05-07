9790e5416a52cef581897118a976911851302eff
Govind Thakur <govind8thakur@gmail.com>
2026-05-07 15:09:16 +0530

    refactor: clean up plan route code for better readability

--- server/src/routes/plan.ts
+++ server/src/routes/plan.ts
@@ -3,24 +3,32 @@ import { PresentationIcon } from "lucide-react";
 import { error, log } from "node:console";
 import { Prisma } from "../../generated/prisma/browser";
 import { prisma } from "../lib/prisma";
+import { version } from "node:os";
 
 export const planRouter = Router();
 
-planRouter.post("/",async(req:Request,res:Response)=>{
-try {
-    const {userId} = req.body;
-    if(!userId){
-        return res.status(400).json({error: "User id is required"});
+planRouter.post("/", async (req: Request, res: Response) => {
+    try {
+        const { userId } = req.body;
+        if (!userId) {
+            return res.status(400).json({ error: "User id is required" });
+        }
+        const profile = await prisma.user_profiles.findUnique({
+            where: { user_id: userId },
+        });
+        if (!profile) {
+            return res.status(400).json({ error: "User profile   not found. Complete onBorading first" });
+        }
+
+        const latestPlan = await prisma.training_plans.findFirst({
+            where: { user_id: userId },
+            orderBy: { ceated_at: "desc" },
+            select: { version: true }, 
+
+        })
+
+    } catch (error) {
+        console.log("Error generating plan ", error);
+        res.status(500).json({ error: "Failed to generate plan" });
     }
-    const profile = await prisma.user_profiles.findUnique({
-        where:{user_id:userId},
-    });
-    if(!profile){
-        return res.status(400).json({error: "User profile   not found. Complete onBorading first"});
-    }
-    
-} catch (error) {
-    console.log("Error generating plan ", error);
-    res.status(500).json({error:"Failed to generate plan"});
-}
 })
